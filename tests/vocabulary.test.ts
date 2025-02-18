aaa692266ff2cffc2822db96f70e22337a606120
lolotrgeek <jon@simplyenable.com>
2025-02-18 16:15:32 -0600

    fix vocab tests

--- tests/vocabulary.test.ts
+++ tests/vocabulary.test.ts
@@ -2,11 +2,31 @@
 import { generateKeyVocabulary, generateStringVocabulary, findInVocabulary, findKeyInVocabulary } from '../src/vocabulary';
 
 describe('generateKeyVocabulary', () => {
+    it('should handle an empty array', () => {
+        const obj: any[] = [];
+        const vocabulary = generateKeyVocabulary(obj);
+        expect(vocabulary).toEqual({});
+    });
+
+    it('should handle an empty object', () => {
+        const obj = {};
+        const vocabulary = generateKeyVocabulary(obj);
+        expect(vocabulary).toEqual({});
+    });
+
     it('should generate a vocabulary for the keys of an object', () => {
         const obj = { a: 1, b: 'hello', c: { d: 2, e: 'world' } };
-        const vocabulary = generateKeyVocabulary(obj);
-        expect(vocabulary).toEqual({ a: 0, b: 1, c: 2 });
+        const flat_obj = { a: 1, b: 'hello', 'c.d': 2, 'c.e': 'world' };
+        const vocabulary = generateKeyVocabulary(flat_obj);
+        expect(vocabulary).toEqual({ a: 0, b: 1, 'c.d': 2, 'c.e': 3 });
     });
+
+    it('should generate a vocabulary for the keys of an array', () => {
+        const obj = [{ a: 1, b: 'hello' }, { c: 2, d: 'world' }];
+        const flat_obj = { '0.a': 1, '0.b': 'hello', '1.c': 2, '1.d': 'world' };
+        const vocabulary = generateKeyVocabulary(flat_obj);
+        expect(vocabulary).toEqual({ '0.a': 0, '0.b': 1, '1.c': 2, '1.d': 3 });
+    })
 });
 
 describe('generateStringVocabulary', () => {
