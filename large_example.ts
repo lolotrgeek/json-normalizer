62e16da09c24e6ff7ee45e0a9b87afdeaf652c85
lolotrgeek <jon@simplyenable.com>
2025-02-22 13:07:44 -0600

    benchmark large example

--- large_example.ts
+++ large_example.ts
@@ -9,10 +9,12 @@ import { denormalize, mergeDatasetAndFindMinMax } from "./src/normalizer";
 const filePath = path.join(__dirname, "largeArray.json");
 const fileContent = fs.readFileSync(filePath, "utf-8");
 
+const startTime = performance.now();
+
 // Parse JSON as an array.
 const dataArray: any[] = JSON.parse(fileContent);
 
-// Process each object. (best not to encode all at once, encode each object separately and have multiple vocabualries)
+// Process each object. (best not to encode all at once, encode each object separately and have multiple vocabularies)
 const encoded = dataArray.map((obj, index) => {
     // Flatten the object.
     const flatObj = flattenObject(obj);
@@ -27,9 +29,12 @@ const minMaxIndex = mergeDatasetAndFindMinMax(encoded.map((obj) => obj.triples))
 
 const normalized = encoded.map(obj => normalize(obj.triples, minMaxIndex));
 // console.log(normalized)
+const endTime = performance.now();
 
 const denormalized = normalized.map(obj => denormalize(obj, minMaxIndex))
 
 const decoded = denormalized.map((obj, index) => unFlattenObject(decodeObject(obj, encoded[index].keyVocabulary, encoded[index].stringVocabulary)));
 
-console.log(decoded)
+
+console.log(decoded);
+console.log(`Time taken: ${endTime - startTime} ms`);
